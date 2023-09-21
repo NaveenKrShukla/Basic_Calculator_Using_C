4f8c3a61b36876fe8a7e073f9441cb3766560bd8
Naveen Shukla <Naveen.22b0121095@abes.ac.in>
2023-09-21 11:30:44 +0530

    add calculator file

---
+++ CalculationInC.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Hey! I have created a Calcultaor.Do you want to see that?\n");
+    printf("Lets begun with sumation of two numbers\n");
+    printf("Ente the number a\n");
+    scanf("%d",&a);
+    printf("Enter the number b\n");
+    scanf("%d",&b);
+    printf("The sum is:%d\n", a+b); 
+    printf("Now Lets try substraction\n");
+    printf("Ente the number a\n");
+    scanf("%d",&a);
+    printf("Enter the number b\n");
+    scanf("%d",&b);
+    printf("The diffrence is:%d\n", a-b);
+    printf("Now Lets try Multiplication As well\n");
+    printf("Enter the number a\n");
+    scanf("%d",&a);
+    printf("Enter the number b\n");
+    scanf("%d",&b);
+    printf("The multiplication is:%d\n",a*b);
+    printf("Want to try division as well?\n");
+    printf("Enter the divident \n");
+    scanf("%d",&a);
+    printf("Enter the divisor\n");
+    scanf("%d",&b);
+    printf("The quotient is:%d\n",a/b );
+    printf("ThankYou For Using My Calculator :)");
+    return 0;
+
+
+}
